98c3eb39f324cfe9ce7a48807be31a54bfd617e9
Marton Bocsi <mvbocsi@gmail.com>
2024-03-03 17:11:58 -0600

    Added search bars

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import SearchBar from "@/components/searchbar";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative flex place-items-center before:opacity-70 before:blur-2xl after:blur-2xl after:-translate-x-1/3 before:absolute before:h-[200px] before:w-full sm:before:w-[240px] before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[360px] after:bg-gradient-conic after:from-sky-300 after:via-blue-300 after:content-[''] before:dark:from-black before:dark:to-transparent before:dark:opacity-30 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-50 before:lg:h-[200px] z-[-1]">
-        <h1 className="text-6xl">Vyltix</h1>
+    <main className="flex min-h-screen flex-col items-center justify-around p-24 pt-36">
+      <div className="w-full flex flex-col items-center gap-16">
+        <div className="relative flex place-items-center before:opacity-70 before:blur-2xl after:blur-2xl after:-translate-x-1/3 before:absolute before:h-[200px] before:w-full sm:before:w-[240px] before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[360px] after:bg-gradient-conic after:from-sky-300 after:via-blue-300 after:content-[''] before:dark:from-black before:dark:to-transparent before:dark:opacity-30 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-50 before:lg:h-[200px] z-[-1]">
+          <h1 className="text-6xl">Vyltix</h1>
+        </div>
+        <SearchBar href="/" placeholder="Search by event, artist, or venue" />
       </div>
+      <div></div>
     </main>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,44 @@
 import Link from "next/link";
+import { FaSearch } from "react-icons/fa";
+import { redirect } from "next/navigation";
 
 export default function Navbar() {
+  async function search(formData: FormData) {
+    "use server";
+
+    redirect(`$/search?query=${formData.get("query")}`);
+  }
   return (
-    <nav className="fixed top-0 h-12 w-full flex flex-row justify-between items-center px-32 bg-white bg-opacity-50 dark:bg-zinc-950 dark:bg-opacity-60 backdrop-blur-md border-b border-zinc-200 dark:border-zinc-900">
-      <Link href="/">Vyltix</Link>
+    <nav className="fixed top-0 h-12 w-full flex flex-row gap-4 justify-between items-center px-32 bg-white bg-opacity-50 dark:bg-zinc-950 dark:bg-opacity-60 backdrop-blur-md border-b border-zinc-200 dark:border-zinc-900">
+      <Link href="/">
+        <p className="text-2xl">Vyltix</p>
+      </Link>
+      <div className="w-full flex flex-row justify-between items-center">
+        <div className="flex flex-row gap-8 items-center">
+          <form
+            action={search}
+            className="flex flex-row items-center border border-zinc-300 dark:border-zinc-800 rounded-full"
+          >
+            <button
+              type="submit"
+              value="Submit"
+              className="rounded-ful p-3 duration-200 ml-0"
+            >
+              <FaSearch size="1em" />
+            </button>
+            <input
+              name="query"
+              className="focus:outline-none bg-transparent pr-2"
+              placeholder="Search by event, artist, or venue"
+            ></input>
+          </form>
+
+          <Link href="/concerts">Concerts</Link>
+          <Link href="/artists">Artists</Link>
+          <Link href="/venues">Venues</Link>
+        </div>
+        <Link href="/login">Login</Link>
+      </div>
     </nav>
   );
 }
