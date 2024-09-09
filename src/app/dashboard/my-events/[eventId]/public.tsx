6820bccb1906dfcabe6f7ccc3fd1fce373ef7f49
Marton Bocsi <mvbocsi@gmail.com>
2024-09-09 12:56:26 -0500

    Added alert to confirm making event public.

--- src/app/dashboard/my-events/[eventId]/public.tsx
+++ src/app/dashboard/my-events/[eventId]/public.tsx
@@ -2,6 +2,17 @@
 
 import { Button } from "@/components/ui/button";
 import { setEventPublic } from "./actions";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 export default function PublicToggle({
   pub,
@@ -14,9 +25,28 @@ export default function PublicToggle({
     <div className="inline-flex items-center justify-between space-x-4">
       <p className="text-2xl">Change Event Visibility</p>
       {!pub ? (
-        <Button variant="destructive" onClick={() => setEventPublic(true, id)}>
-          Make event public
-        </Button>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button variant="default">Make event public</Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Are you sure you want to make this event public?
+              </AlertDialogTitle>
+              <AlertDialogDescription>
+                This action can not be undone. Making major changes to this
+                event once it is public is limited.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={() => setEventPublic(true, id)}>
+                Make public
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       ) : (
         <Button variant="outline" disabled>
           This event is already public
